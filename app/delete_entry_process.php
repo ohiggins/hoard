8534f26be45e621d2fd19be20de5acee0076dbea
Adam Greenough <adam@amazorize.com>
2017-03-26 03:42:52 +0100

    Fixed delete entry

---
+++ app/delete_entry_process.php
@@ -0,0 +1,43 @@
+<?php
+	
+/*******************************
+	
+	Process Edit Entry
+	app/edit_process.php
+	
+********************************/
+
+require_once('login_check.php');
+include('functions.php');
+
+$user = new User();
+$user->set_id($current_user['user_id']); 
+	
+$id = trim($_POST['entryid']);
+$entry = new Entry();
+$entry->set_id($id); 
+	
+$snippet = new Snippet();
+$snippet->set_id($entry->get_parent()); 
+	
+// Double check the current user owns this snippet
+if($snippet->get_author() != $user->get_id()) {
+	die('Permission denied');	
+}
+
+// Connect to database and insert entry
+require_once('db.php');
+
+$entry_id = $entry->get_id();
+
+$new_entry = $mysqli->query("DELETE FROM snippets_entries WHERE entry_id = $entry_id");
+if (!$new_entry) {
+	die('error deleting entry: '.$mysqli->error);
+}
+
+// Redirect back to snippet page with success message
+header('Location: ../snippet.php?id=' . $snippet->get_id() . '&delete_success');
+
+?>
+
+
---
+++ app/edit_process.php
@@ -0,0 +1,56 @@
+<?php
+	
+/*******************************
+	
+	Process Edit Entry
+	app/edit_process.php
+	
+********************************/
+
+require_once('login_check.php');
+include('functions.php');
+
+$user = new User();
+$user->set_id($current_user['user_id']); 
+	
+$id = trim($_POST['entryid']);
+$entry = new Entry();
+$entry->set_id($id); 
+	
+$snippet = new Snippet();
+$snippet->set_id($entry->get_parent()); 
+	
+// Double check the current user owns this snippet
+if($snippet->get_author() != $user->get_id()) {
+	die('Permission denied');	
+}
+
+// Double check the fields have data
+if (!isset($_POST['title']) || trim($_POST['title']) == '') {
+	die('you forgot to put in a title');
+}
+
+if (!isset($_POST['entry']) || trim($_POST['entry']) == '') {
+	die('you forgot to put in an entry');
+}
+
+// Connect to database and insert entry
+require_once('db.php');
+
+$new_entry_snippet = "'".$mysqli->escape_string(trim($_POST['snippet']))."'";
+$new_entry_content = "'".$mysqli->escape_string(trim($_POST['entry']))."'";
+$new_entry_title = "'".$mysqli->escape_string(trim($_POST['title']))."'";
+$new_entry_language = "'".$mysqli->escape_string(trim($_POST['language']))."'";
+$entry_id = $entry->get_id();
+
+$new_entry = $mysqli->query("UPDATE snippets_entries SET entry_content = $new_entry_content, entry_name = $new_entry_title, entry_language = $new_entry_language WHERE entry_id = $entry_id");
+if (!$new_entry) {
+	die('error editing entry: '.$mysqli->error);
+}
+
+// Redirect back to snippet page with success message
+header('Location: ../snippet.php?id=' . $entry->get_parent() . '&edit_success');
+
+?>
+
+
