e05ad0e383581abc3e6eef4385b9744b15d75401
Adam Greenough <adam@amazorize.com>
2017-03-28 04:38:57 +0100

    Wrote some dirty, dirty code to make label picker prettier. Let's make a note to come back to that with fresh eyes.

--- details.php
+++ details.php
@@ -31,7 +31,7 @@ $snippet->set_id($id);
     <!-- Content Header (Page header) -->
     <section class="content-header" style="background: red; height: 50px;">
       <h1>
-        Edit Entry
+        Edit Snippet Details
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -47,7 +47,6 @@ if($snippet->get_author() == $user->get_id()) {
 ?>
 
 <div id="register-prompt">
-<h1>edit snippet entry</h1>
 <form action="app/details_process.php" method="post">
 <input name="snippetid" type="hidden" value="<?php echo $snippet->get_id(); ?>" />
 <p><label>Snippet Title:</label> <input tabindex="1" name="title" value="<?php echo htmlspecialchars($snippet->get_title()); ?>" type="text" placeholder="test" /></p>
@@ -70,7 +69,7 @@ if($snippet->get_author() == $user->get_id()) {
                 
              <p><label>Snippet Labels:</label>
 	             <?php include('app/db.php');
-							$labels = mysqli_query($mysqli, "SELECT * FROM labels");
+							$labels = mysqli_query($mysqli, "SELECT * FROM labels ORDER BY label_name");
 							if ($labels AND $labels->num_rows !== 0) { ?>
 	             <select name="labelpicker[]" size="1" id="labels" class="labelpicker form-control" multiple>
                 	    <?php
@@ -81,10 +80,25 @@ if($snippet->get_author() == $user->get_id()) {
 								if ($label->has_label($snippet->get_id()) == true) { $active = 'selected="selected"'; } else { $active = ''; }
 								echo "<option " . $active . " title='label-" . $row_labels['label_id'] . "' value='" . $row_labels['label_id'] . "'>" . $row_labels['label_name'] . "</option>";
 							} 
+							?>
+	             </select>
+	             <?php
 							} else {
 								echo 'Please add a label!';
 							}
 							
+							
+							// Generate a mini CSS snippet to style label chooser.
+							// I am not proud of the following 8 lines of code. It's late, it works, it'll do for now!
+							echo '<style>';
+							$labels = mysqli_query($mysqli, "SELECT * FROM labels");
+							while ($row_labels = mysqli_fetch_array($labels, MYSQLI_ASSOC)) {
+								$labelid = $row_labels['label_id'];
+								$label = new Label();
+								$label->set_id($labelid); 
+								echo 'li.select2-selection__choice[title="label-' . $label->get_id() . '"] { background-color: ' . $label->get_hex() . ' !important; border-color: ' . $label->get_hex() . ' !important; }';
+							} 
+							echo '</style>';
 						?>
 	             </select>
 
