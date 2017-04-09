26c620d90c16b67652c84ae86f704ee3ffbd9964
Adam Greenough <adam@amazorize.com>
2017-04-09 01:21:02 +0100

    Label search finally working properly

--- app/search_process.php
+++ app/search_process.php
@@ -7,21 +7,23 @@
 	
 ********************************/
 
-// Get the label via hashtag
-
+// Define our special search terms (#, @, ^, <3)
 $search_label = $search->search_label(); 
 $search_author = $search->search_author();
 $search_order = $search->search_order();
 
+// Strip out everything else and leave us with the primary search string
 $search_contents = $mysqli->escape_string(trim(str_replace(
     array($search_label, $search_author, $search_order, '#', '^', '@', '+', '&lt;3'),
     array('','','','','','',''), 
     $q
 )));
+
+// The primary search query
 $contents_search = "SELECT * FROM snippets WHERE snippet_title LIKE '%$search_contents%'";
 
 
-
+// Label Search -------------------
 
 // Get label from search query (Anything between a # and a space)
 $search_label = $search->search_label();
@@ -30,15 +32,22 @@ $search_label = $search->search_label();
 $label_search = null;
 $label_results = null;
 
-
+// If a label search detected...
 if($search_label) {
-	$label_query = mysqli_query($mysqli, "SELECT snippet_id FROM labels WHERE label_name LIKE '%$search_label%'");
-	if($label_query) { $label_results = mysqli_fetch_array($label_query, MYSQLI_ASSOC); }
-	if($label_query) { $label_id = htmlentities($label_results['label_id']); } else { $label_id = null; }
-	$label_search = " AND snippet_id IN (SELECT (snippet_id) FROM tagging WHERE label_id = $label_id)";
+	
+	// Get the label from our labels database
+	$label_query = mysqli_query($mysqli, "SELECT * FROM labels WHERE label_name LIKE '%$search_label%' LIMIT 1");
+	$label_results = mysqli_fetch_array($label_query, MYSQLI_ASSOC);
+	
+	// If it's not found, run a query we know will fail to show 0 results (TODO: a better way of handling this?)
+	if($label_query) { $label_id = htmlentities($label_results['label_id']); } else { $label_id = "'$search_label'"; }
+	
+	// Create our bit of the main query
+	$label_search = " AND snippet_id IN (SELECT (snippet_id) FROM tagging WHERE label_id = '$label_id')";
 }
 
 
+// User Search -------------------
 /**
 // Grab user from search, work out what ID that user has, fish those snippet ID's out of snippets and write our query
 $search_author = $search->search_author(); 
@@ -51,10 +60,9 @@ if ($user_id) { $author_search = " AND snippet_author = $user_id"; } else { $aut
 }
 **/
 
+// Assemble our final query
 $final_search = $contents_search . $label_search;
-
 $snippets = mysqli_query($mysqli, "$final_search");
-
 echo $final_search;
 
 ?>
