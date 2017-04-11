0fe050c646a50fe9d662b4a81461a20fd5aac782
Adam Greenough <adam@amazorize.com>
2017-04-12 00:00:58 +0100

    Admin progress

--- admin.php
+++ admin.php
@@ -35,6 +35,10 @@
               <li class=""><a href="#tab_3" data-toggle="tab" aria-expanded="false">Add User</a></li>
             </ul>
             <div class="tab-content">
+	            
+	           
+	           
+	          <!-- Admin Settings -->
               <div class="tab-pane active" id="tab_1">
 						<div class="box-body">
 							<label>Company Name</label>
@@ -60,16 +64,62 @@
 							
               </div>
               <!-- /.tab-pane -->
+              
+              
+              
+              <!-- User Permissions -->
               <div class="tab-pane" id="tab_2">
-                The European languages are members of the same family. Their separate existence is a myth.
-                For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
-                in their grammar, their pronunciation and their most common words. Everyone realizes why a
-                new common language would be desirable: one could refuse to pay expensive translators. To
-                achieve this, it would be necessary to have uniform grammar, pronunciation and more common
-                words. If several languages coalesce, the grammar of the resulting language is more simple
-                and regular than that of the individual languages.
+	              
+	              
+	              <table class="table table-bordered">
+                <tbody><tr>
+                  <th style="width: 10px">#</th>
+                  <th>Task</th>
+                  <th>Progress</th>
+                  <th style="width: 40px">Label</th>
+                </tr>
+
+	              
+			<?php	
+
+			$authors = mysqli_query($mysqli, "SELECT * FROM users");
+				
+			while ($row_authors = mysqli_fetch_array($authors, MYSQLI_ASSOC)) {
+				$author = new User();
+				$author->set_id($row_authors['user_id']);
+			?>
+			
+			     <tr>
+                  <td><img class="img-circle" src="<?php echo $author->get_gravatar(); ?>" alt="User Avatar"></td>
+                  <td>Fix and squish bugs</td>
+                  <td>
+<?php echo $author->get_name(); ?>
+                  </td>
+                  <td><?php echo $author->get_id(); ?></td>
+                </tr>
+			
+					<?php } ?>
+			
+			              </tbody></table>
               </div>
               <!-- /.tab-pane -->
+              
+              
+              
+              
+              
+              <!-- Add User -->
+              <div class="tab-pane" id="tab_3">
+<form action="app/add_user_process.php" method="post">
+<p><label>Your Email:</label> <input tabindex="1" id="start-here" name="e" type="email" placeholder="email address" /></p>
+<p><label>Your Password:</label> <input tabindex="2" name="p1" type="password" /></p>
+<p><label>Your Password:</label> <input tabindex="3" name="p2" type="password" /></p>
+<p><input tabindex="5" type="submit" value="sign up &raquo;" /></p>
+</form>
+              </div>
+              <!-- /.tab-pane -->
+              
+              
             </div>
             <!-- /.tab-content -->
           </div>
